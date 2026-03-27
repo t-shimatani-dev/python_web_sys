e02069a58c1a6ca571c32fd2349f9ef58ba3a082
Takayuki Shimatani <agio0021@gmail.com>
2026-03-27 22:02:22 +0900

    refactor: modified utils/exceptions.py.

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,20 +1,20 @@
 # utils/exceptions.py
 
 class EmployeeSystemException(Exception):
-    """システム共通の基底例外クラス"""
+    """従業員システムの基本例外クラス。"""
     def __init__(self, message, error_code=None):
         self.message = message
         self.error_code = error_code
         super().__init__(self.message)
 
 class DatabaseException(EmployeeSystemException):
-    """データベース関連の例外"""
+    """データベース関連の例外クラス。"""
     pass
 
 class ValidationException(EmployeeSystemException):
-    """バリデーション関連の例外"""
+    """入力検証関連の例外クラス。"""
     pass
 
 class NotFoundException(EmployeeSystemException):
-    """データ不存在の例外"""
+    """リソースが見つからない場合の例外クラス。"""
     pass
